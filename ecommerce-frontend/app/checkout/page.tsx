c8afae2c9808c56dca09bf62f8ab92cb3040cfe0
Abel Guta <gutaabel546@gmail.com>
2025-11-02 21:51:53 +0300

    Checkout page styling

--- ecommerce-frontend/app/checkout/page.tsx
+++ ecommerce-frontend/app/checkout/page.tsx
@@ -13,36 +13,40 @@ export default function page() {
 
   if (total === 0 || items.length === 0) {
     return (
-      <div>
-        <h2>Your Cart is Empty.</h2>
+      <div className="max-w-[80%] mx-auto px-4 py-8 text-center">
+        <h2 className="text-3xl font-bold mb-4">Your Cart is Empty.</h2>
       </div>
     );
   }
   return (
-    <div>
-      <h1>Checkout</h1>
-      <Card>
+    <div className="max-w-[80%] mx-auto px-4 py-8">
+      <h1 className="text-3xl font-bold mb-8 text-center">Checkout</h1>
+      <Card className="max-w-md mx-auto mb-8">
         <CardHeader>
-          <CardTitle>Order Summary</CardTitle>
+          <CardTitle className="text-xl font-bold">Order Summary</CardTitle>
         </CardHeader>
         <CardContent>
-          <ul>
+          <ul className="space-y-4">
             {items.map((item, key) => (
-              <li key={key}>
-                <div>
-                  <span>{item.name}</span>
-                  <span>{(item.price * item.quantity).toFixed(2)}</span>
+              <li key={key} className="flex flex-col gap-2 border-b pb-2">
+                <div className="flex justify-between">
+                  <span className="font-medium">{item.name}</span>
+                  <span className="font-semibold">
+                    {(item.price * item.quantity).toFixed(2)}
+                  </span>
                 </div>
 
-                <div className="flex space-x-4 items-center mt-3">
+                <div className="flex items-center gap-2">
                   <Button
                     onClick={() => removeItem(item.id)}
+                    size="sm"
                     variant={"outline"}
                   >
                     -
                   </Button>
                   <span className="text-lg font-semibold">{item.quantity}</span>
                   <Button
+                    size="sm"
                     onClick={() => {
                       addItem({
                         ...item,
@@ -56,8 +60,17 @@ export default function page() {
               </li>
             ))}
           </ul>
+
+          <div className="mt-4 border-t pt-2 text-lg font-semibold">
+            Total:${total.toFixed(2)}.
+          </div>
         </CardContent>
       </Card>
+      <form action="" className="max-w-md mx-auto">
+        <Button type="submit" variant={"default"}>
+          Proceed To Payment
+        </Button>
+      </form>
     </div>
   );
 }
